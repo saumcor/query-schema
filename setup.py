c7d3ed93e5515a62d40fbd89d088823a488eefa2
AbdealiJK <abdealikothari@gmail.com>
2022-10-13 15:05:38 +0530

    Cleanup requirements and install_requires
    We keep requirements as developer requirements (linters, tests, etc)
    We keep the install_requires as the runtime dependencies required.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ BASE_PATH = os.path.abspath(os.path.dirname(__file__))
 if __name__ == '__main__':
     setup(
         name='query-schema',
-        install_requires=[],
+        install_requires=['marshmallow', 'marshmallow-sqlalchemy', 'SQLAlchemy'],
         packages=['query_schema'],
         classifiers=[
             'Programming Language :: Python :: 3',
