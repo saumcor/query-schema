08eda3c426702dc84d25bd033947c41d7c4412ce
AbdealiJK <abdealikothari@gmail.com>
2022-10-13 14:59:06 +0530

    Add setup.py to create the python package

---
+++ setup.py
@@ -0,0 +1,16 @@
+import os
+
+from setuptools import setup
+
+BASE_PATH = os.path.abspath(os.path.dirname(__file__))
+
+if __name__ == '__main__':
+    setup(
+        name='query-schema',
+        install_requires=[],
+        packages=['query_schema'],
+        classifiers=[
+            'Programming Language :: Python :: 3',
+            'Operating System :: OS Independent',
+        ],
+    )
